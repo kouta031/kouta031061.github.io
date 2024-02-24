cc0389d1c661f16cd0f7f725fa22e42d7c495088
kouta031 <136604934+kouta031@users.noreply.github.com>
2024-02-24 11:19:05 +0900

    Update main_t2.js

--- main_t2.js
+++ main_t2.js
@@ -22,3 +22,28 @@ window.addEventListener("DOMContentLoaded", () => {
     createStar();
   }
 });
+
+window.addEventListener("load", function () {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const area = document.querySelector(".js-area");
+  const wrap = document.querySelector(".js-wrap");
+  const items = document.querySelectorAll(".js-item");
+  const num = items.length;
+
+  gsap.set(wrap, { width: num * 100 + "%" });
+  gsap.set(items, { width: 100 / num + "%" });
+
+  gsap.to(items, {
+      xPercent: -100 * (num - 1),
+      ease: "none",
+      scrollTrigger: {
+          trigger: area,
+          start: "top top",
+          end: "+=1000",
+          pin: true,
+          scrub: true,
+      }
+  });
+});
